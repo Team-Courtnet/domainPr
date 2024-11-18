355e6a2adb0780cac6d5195573f37d69517b020f
jay91537 <veuxft@gmail.com>
2024-11-19 01:24:16 +0900

    Feat : Calculator v2

--- src/main/java/com/domainpr/calculator/CalculatorService.java
+++ src/main/java/com/domainpr/calculator/CalculatorService.java
@@ -14,32 +14,43 @@ public class CalculatorService {
 
     public List<Integer> getStringArray(String stringArray) throws Exception {
 
-        // service layer에 모든 조건, 예외처리, 유효성 검증을 몰아 넣음 - 도메인 필요없군
-        String distinguisher = "[,:]";
-        String numbers = stringArray;
+//        // service layer에 모든 조건, 예외처리, 유효성 검증을 몰아 넣음 - 도메인 필요없군
+//        String distinguisher = "[,:]";
+//        String numbers = stringArray;
+//
+//        if (stringArray.startsWith("//")){
+//            int distinguisherIndex = stringArray.indexOf("\n");
+//            distinguisher = stringArray.substring(2, distinguisherIndex);
+//            numbers = stringArray.substring(distinguisherIndex+1);
+//        }
+//
+//        String[] numberTokens = numbers.split(distinguisher);
+//        List<Integer> result = new ArrayList<>();
+//
+//        for(String numberToken : numberTokens) {
+//            try {
+//                int number = Integer.parseInt(numberToken);
+//                if (number < 0) { // 음수인지 확인
+//                    throw new RuntimeException("숫자가 아닌 값이나 음수를 입력할 수 없습니다."); // 음수일 경우 예외 발생
+//                }
+//                result.add(number);
+//            } catch (RuntimeException e) {
+//                throw new RuntimeException("숫자가 아닌 혹은 음수를 입력할 수 없습니다.");
+//            }
+//        }
+//
+//        return result;
+//    }
 
-        if (stringArray.startsWith("//")){
-            int distinguisherIndex = stringArray.indexOf("\n");
-            distinguisher = stringArray.substring(2, distinguisherIndex);
-            numbers = stringArray.substring(distinguisherIndex+1);
-        }
+    // domain 계층에서 유효성 검증을 해보자
+        Calculator calculator = new Calculator(stringArray);
 
-        String[] numberTokens = numbers.split(distinguisher);
-        List<Integer> result = new ArrayList<>();
+        // 다른 구분자가 쓰이는지 검증
+        ArrayList<String> fixedDistinguisherAndNumbers = calculator.ifAnotherDistinguisher(stringArray);
 
-        for(String numberToken : numberTokens) {
-            try {
-                int number = Integer.parseInt(numberToken);
-                if (number < 0) { // 음수인지 확인
-                    throw new RuntimeException("숫자가 아닌 값이나 음수를 입력할 수 없습니다."); // 음수일 경우 예외 발생
-                }
-                result.add(number);
-            } catch (RuntimeException e) {
-                throw new RuntimeException("숫자가 아닌 혹은 음수를 입력할 수 없습니다.");
-            }
-        }
+        // 입력값에 숫자가 아닌 값 혹은 음수가 있는지 검증
+        return calculator.ifNotNumberOrNegative(fixedDistinguisherAndNumbers);
 
-        return result;
-    }
+}
 
 }
